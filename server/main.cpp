9242b42de7c633a56844d63884bd6cf002f0f825
Łukasz Radziak <lukasilestam@gmail.com>
2022-01-30 00:49:15 +0100

    Refactor Wifi server

--- server/main.cpp
+++ server/main.cpp
@@ -1,8 +1,10 @@
 #include <Arduino.h>
-
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
 #include "SPIFFS.h"
+
+#define WIFI_SSID "VolvoMonitor"
+#define WIFI_PASS "12345678"
 
 AsyncWebServer server(80);
 
@@ -10,41 +12,21 @@ void setup() {
   Serial.begin(115220);
   printf("Hello Volvo Monitor!\n");
 
-  if(!SPIFFS.begin(true)){
-    printf("An Error has occurred while mounting SPIFFS\n");
-		return;
-	}
-
+  SPIFFS.begin(true);
   WiFi.mode(WIFI_STA);
-  WiFi.softAP("VolvoMonitor", "12345678");
+  WiFi.softAP(WIFI_SSID, WIFI_PASS);
 
   IPAddress IP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(IP);
+  printf("Address ip: %s\n", IP.toString().c_str());
 
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/index.html", "text/html");
-  });
-
-  server.on("/favicon.ico", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/favicon.ico", "image/x-icon");
-  });
-
-  server.on("/index.css", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/index.css", "text/css");
-  });
-
-  server.on("/index.js", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/index.js", "text/javascript");
-  });
-
-  server.on("/vendor.js", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/vendor.js", "text/javascript");
-  });
+  server
+    .serveStatic("/", SPIFFS, "/")
+    .setDefaultFile("index.html")
+    .setCacheControl("max-age=600");
 
   server.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 }
