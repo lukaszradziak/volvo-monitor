62071f07e5c02acd91761cc666f608a16f24fd8a
Łukasz Radziak <lukasilestam@gmail.com>
2022-01-30 20:41:12 +0100

    Testing string buffer

--- client/components/pages/Monitor.jsx
+++ client/components/pages/Monitor.jsx
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react";
+
 const Monitor = () => {
-  return <p>Monitor Page</p>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    let timer;
+
+    const loop = async () => {
+      let request = await fetch(`http://192.168.4.1/api/monitor-data`);
+      let text = await request.text();
+      let lines = text
+        .trim()
+        .split("\n")
+        .filter((line) => line);
+
+      lines.forEach((line) => {
+        let values = line.split(";");
+        setData((data) => [...data, values]);
+      });
+
+      timer = setTimeout(loop, 100);
+    };
+    loop();
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  return (
+    <div>
+      <p>Monitor Page {data.length}</p>
+      {data.length ? <p>Data: {data[data.length - 1][0]}</p> : null}
+    </div>
+  );
 };
 
 export default Monitor;
--- server/main.cpp
+++ server/main.cpp
@@ -7,24 +7,48 @@
 #define WIFI_PASS "12345678"
 
 AsyncWebServer server(80);
+String monitorData = "";
+
+void task0(void* param){
+  while(true){
+    if(monitorData.length() > 10000){
+      monitorData = "";
+    }
+    monitorData += millis();
+    monitorData += ";";
+    monitorData += "test";
+    monitorData += "\n";
+
+    vTaskDelay(100UL);
+  }
+}
 
 void setup() {
   Serial.begin(115220);
   printf("Hello Volvo Monitor!\n");
 
   SPIFFS.begin(true);
+  WiFi.setSleep(false);
   WiFi.mode(WIFI_STA);
   WiFi.softAP(WIFI_SSID, WIFI_PASS);
 
   IPAddress IP = WiFi.softAPIP();
   printf("Address ip: %s\n", IP.toString().c_str());
 
+  server.on("/api/monitor-data", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plain", monitorData);
+    monitorData = "";
+  });
+
   server
     .serveStatic("/", SPIFFS, "/")
     .setDefaultFile("index.html")
     .setCacheControl("max-age=600");
 
+  DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
   server.begin();
+
+  xTaskCreatePinnedToCore(task0, "Task0", 4096, NULL, 1, NULL, 0);
 }
 
 void loop() {
