caf70d435fe1bb02124499e2a6ab08a8471f06cc
Łukasz Radziak <lukasilestam@gmail.com>
2022-01-30 22:23:17 +0100

    Add deploy to Github Pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.BASE_PATH ? process.env.BASE_PATH : "./",
   resolve: {
     alias: {
       "@": resolve("client"),
